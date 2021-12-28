110390c46a73b61660f879f3536b3ddc66c9c3e3
Doğukan <dogukanatalay46@gmail.com>
2021-12-29 00:53:01 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const profile = new Profile();
+const ui = new UI();
+
+const searchProfile = document.getElementById('searchProfile');
+
+searchProfile.addEventListener('keyup',(event)=>{
+    ui.clear();
+    let text = event.target.value;
+
+    if(!text == ''){
+        profile.getProfile(text)
+        .then(res=>{
+            if(res.profile.length === 0){
+                ui.showAlert(text);
+            }
+            else{
+               ui.showProfile(res.profile[0]);
+               ui.showTodo(res.todo);
+            }
+        }).catch(err=>{
+                ui.showAlert(text);
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
